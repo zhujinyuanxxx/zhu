6ad128f838dd0bb61f43038e810b04b9e62a8769
zhujinyuanxxx <3493557231@qq.com>
2023-07-18 14:34:24 +0800

    添加文件

---
+++ test/src/main/java/com/Controller/testController.java
@@ -0,0 +1,24 @@
+package com.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.testoo;
+
+@RestController
+public class testController {
+	
+	@GetMapping("/hello")
+	public String test() {
+		return "hello";
+	}
+	
+	@GetMapping("/hello1")
+	public testoo test1() {
+		testoo t = new testoo();
+		t.setA(1);
+		t.setB(2);
+		System.out.println(t);
+		return t;
+	}
+}
---
+++ test/src/main/java/com/model/testoo.java
@@ -0,0 +1,14 @@
+package com.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class testoo {
+
+	int a;
+	int b;
+
+	
+}
